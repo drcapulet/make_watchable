2c873d2cc0b3a64ed0293b66c3a2daae63c56a1d
Alex Coomans <alex@alexcoomans.com>
2013-03-18 00:03:10 -0500

    Fix AR MassAssignmentSecurity error

--- lib/make_watchable/watching.rb
+++ lib/make_watchable/watching.rb
@@ -2,5 +2,6 @@ module MakeWatchable
   class Watching < ActiveRecord::Base
     belongs_to :watchable, :polymorphic => true
     belongs_to :watcher, :polymorphic => true
+    attr_accessible :watchable, :watcher
   end
 end
